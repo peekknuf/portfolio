707b82ed9459e01c097e9fafc1e0b9d4ebc2540d
peekknuf <peekknuf@gmail.com>
2023-12-08 21:12:56 +0100

    hide giant descriptions

---
+++ toggledesc.js
@@ -0,0 +1,8 @@
+function toggleDescription() {
+    var description = document.getElementById("description");
+    if (description.style.display === "none") {
+        description.style.display = "block";
+    } else {
+        description.style.display = "none";
+    }
+}
