42e30d1e7a7165bba960f9273113d080577fc743
peekknuf <peekknuf@gmail.com>
2023-12-09 09:47:19 +0100

    make btn work as intended

--- toggledesc.js
+++ toggledesc.js
@@ -1,8 +1,24 @@
-function toggleDescription() {
-    var description = document.getElementById("description");
-    if (description.style.display === "none") {
-        description.style.display = "block";
+document.getElementById('button1').addEventListener('click', function(event) {
+    toggle(event, 'description1');
+});
+document.getElementById('button2').addEventListener('click', function(event) {
+    toggle(event, 'description2');
+});
+document.getElementById('button3').addEventListener('click', function(event) {
+    toggle(event, 'description3');
+});
+document.getElementById('button4').addEventListener('click', function(event) {
+    toggle(event, 'description4');
+});
+
+function toggle(event, descriptionId) {
+    var description = document.getElementById(descriptionId);
+    
+    if (description.style.display === 'none' || description.style.display === '') {
+      event.target.innerText = 'Hide description';
+      description.style.display = 'block';
     } else {
-        description.style.display = "none";
+      event.target.innerText = 'Show full description';
+      description.style.display = 'none';
     }
-}
+  }
