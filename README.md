497b99bd159577c63bf57e76009d9fff691f0015
peekknuf <peekknuf@gmail.com>
2023-12-16 09:19:26 +0100

    no js intended

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 I'm just playing around
+
+ok so inititally there was no intention whatsoever to add a single line of js
--- toggledesc.js
+++ toggledesc.js
@@ -1,24 +1,22 @@
-document.getElementById('button1').addEventListener('click', function(event) {
-    toggle(event, 'description1');
-});
-document.getElementById('button2').addEventListener('click', function(event) {
-    toggle(event, 'description2');
-});
-document.getElementById('button3').addEventListener('click', function(event) {
-    toggle(event, 'description3');
-});
-document.getElementById('button4').addEventListener('click', function(event) {
-    toggle(event, 'description4');
-});
+
+for (let i = 1; i <= 4; i++) {
+  let buttonId = 'button' + i;
+  let descriptionId = 'description' + i;
+  let button = document.getElementById(buttonId);
+  
+  button.addEventListener('click', function(event) {
+      toggle(event, descriptionId);
+  });
+}
 
 function toggle(event, descriptionId) {
-    var description = document.getElementById(descriptionId);
-    
-    if (description.style.display === 'none' || description.style.display === '') {
-      event.target.innerText = 'Hide description';
-      description.style.display = 'block';
-    } else {
-      event.target.innerText = 'Show full description';
-      description.style.display = 'none';
-    }
+  var description = document.getElementById(descriptionId);
+  
+  if (description.style.display === 'none' || description.style.display === '') {
+    event.target.innerText = 'Hide description';
+    description.style.display = 'block';
+  } else {
+    event.target.innerText = 'Show full description';
+    description.style.display = 'none';
   }
+}
